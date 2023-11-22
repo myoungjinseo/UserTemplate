69e081a3ad7da657fa74b2aa7a6d5b279c30f988
MyoungJinSeo <sih5673@gmail.com>
2023-11-22 22:47:04 +0900

    Start Setting

---
+++ src/main/java/com/example/usertemplate/UserTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.usertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usertemplate/UserTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
