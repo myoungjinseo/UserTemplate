5c13bbf2ab6d8e951a38fa6cf209eb6ace9a6572
MyoungJinSeo <sih5673@gmail.com>
2023-11-28 17:42:44 +0900

    feat : loadUserByUsername

---
+++ src/main/java/com/example/usertemplate/api/user/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.example.usertemplate.api.user.service;
+
+import com.example.usertemplate.domain.user.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
