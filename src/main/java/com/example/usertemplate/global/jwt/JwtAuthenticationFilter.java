40b839bed3d113d8dc44ac95000fe71f9a28c565
MyoungJinSeo <sih5673@gmail.com>
2023-11-28 17:39:07 +0900

    feat : doFilterInternal

---
+++ src/main/java/com/example/usertemplate/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package com.example.usertemplate.global.jwt;
+
+import com.example.usertemplate.api.user.service.UserDetailService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+    private final UserDetailService userDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        if (!Objects.isNull(authorization)) {
+            String atk = authorization.substring(7);
+            try {
+                String requestURI = request.getRequestURI();
+                if (!requestURI.equals("/account/reissue")) {
+                    throw new JwtException("토큰을 확인하세요.");
+                }
+                UserDetails userDetails = userDetailService.loadUserByUsername(tokenProvider.getUserEmail(atk));
+                Authentication token = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(token);
+            } catch (JwtException e) {
+                request.setAttribute("exception", e.getMessage());
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
